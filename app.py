9675fa3256cd84b1aa22d07be4e243fab877044f
dheerajkumar1a1a <102639931+dheerajkumar1a1a@users.noreply.github.com>
2023-12-23 21:15:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,20 @@ import requests
 
 st.title('Basic Calculator App')
 
-option=st.selectbox('Operation?', ('Addition','Subtraction','Multiplication','Division'))
+# Create a password input field for the URL
+url = st.text_input('Enter API URL:', type='password')
+
+option = st.selectbox('Operation?', ('Addition', 'Subtraction', 'Multiplication', 'Division'))
 
 st.write('')
 
 st.write('Select the number:')
-x=st.slider("X",0,100,20)
-y=st.slider("Y",0,130,10)
+x = st.slider("X", 0, 100, 20)
+y = st.slider("Y", 0, 130, 10)
 
-inputs={"operation":option,"x":x,"y":y}
+inputs = {"operation": option, "x": x, "y": y}
 
 if st.button('Calculate'):
-    res=requests.post(url="https://dheerajkr1a1a-calculator-docker.hf.space/calculate",data=json.dumps(inputs))
-    st.subheader(f'Response from API={res.text}')
+    # Use the entered URL
+    res = requests.post(url=url, data=json.dumps(inputs))
+    st.subheader(f'Response from API = {res.text}')
