3fa5ab96eb53bd4064d5564848c7ad9dd323b469
dheerajkumar1a1a <102639931+dheerajkumar1a1a@users.noreply.github.com>
2023-12-23 11:36:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import json
+import requests
+
+st.title('Basic Calculator App')
+
+option=st.selectbox('Operation?', ('Addition','Subtraction','Multiplication','Division'))
+
+st.write('')
+
+st.write('Select the number:')
+x=st.slider("X",0,100,20)
+y=st.slider("Y",0,130,10)
+
+inputs={"operation":option,"x":x,"y":y}
+
+if st.button('Calculate'):
+    res=requests.post(url='http://127.0.0.1:8000/calculate',data=json.dumps(inputs))
+    st.subheader(f'Response from API={res.text}')
