94ee585edb5fdbb33a09ced72c2e5d5128842e99
dheerajkumar1a1a <102639931+dheerajkumar1a1a@users.noreply.github.com>
2023-12-23 21:17:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ import requests
 
 st.title('Basic Calculator App')
 
-# Create a password input field for the URL
-url = st.text_input('Enter API URL:', type='password')
+# Try to get the secret key from Streamlit secrets
+try:
+    url = st.secrets["api_url"]
+except KeyError:
+    # If the secret key is not found, provide a default value
+    url = st.text_input('Enter API URL:', type='password', value="default_secret_key")
 
 option = st.selectbox('Operation?', ('Addition', 'Subtraction', 'Multiplication', 'Division'))
 
@@ -18,6 +22,6 @@ y = st.slider("Y", 0, 130, 10)
 inputs = {"operation": option, "x": x, "y": y}
 
 if st.button('Calculate'):
-    # Use the entered URL
+    # Use the entered or default URL
     res = requests.post(url=url, data=json.dumps(inputs))
     st.subheader(f'Response from API = {res.text}')
