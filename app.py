0675214344e94e3867f8d14bece8a36608f97f9c
dheerajkumar1a1a <102639931+dheerajkumar1a1a@users.noreply.github.com>
2023-12-23 21:30:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ st.title('Basic Calculator App')
 
 # Try to get the secret key from Streamlit secrets
 try:
-    url = st.secrets["secret"]
+    url = st.secrets["api_url"]
+    st.success("✅ API key is already provided.")
 except KeyError:
     # If the secret key is not found, provide a default value
-    url = st.text_input('Enter API URL:', type='password')
+    url = st.text_input('Enter API URL:', type='password', value="default_secret_key")
 
 option = st.selectbox('Operation?', ('Addition', 'Subtraction', 'Multiplication', 'Division'))
 
@@ -23,5 +24,5 @@ inputs = {"operation": option, "x": x, "y": y}
 
 if st.button('Calculate'):
     # Use the entered or default URL
-    res = requests.post(url=url, data=json.dumps(inputs))
-    st.subheader(f'Response from API = {res.text}')
+    res = requests.post(url=url, data=json.dumps(inputs
+
